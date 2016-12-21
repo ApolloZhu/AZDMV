d3596f3b1eb569fe98a6e5e8ac8429a4e3e03b3c
ApolloZhu <public-apollonian@outlook.com>
2016-12-21 15:22:18 -0500

    Setup Dependencies

---
+++ DMV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DMV
+//
+//  Created by Apollo Zhu on 12/21/16.
+//  Copyright © 2016 WWITDC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'DMV' do
+  pod 'Kingfisher'
+  pod 'SwiftyJSON'
+  pod 'RealmSwift'
+end
