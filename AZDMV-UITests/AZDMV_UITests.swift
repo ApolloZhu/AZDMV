209bd0ca7531b5ee67be3bb0d5799a49b5bdbd8a
ApolloZhu <public-apollonian@outlook.com>
2020-12-29 20:17:16 -0500

    Screeshot alternative icon feature

--- AZDMV-UITests/AZDMV_UITests.swift
+++ AZDMV-UITests/AZDMV_UITests.swift
@@ -17,10 +17,13 @@ class AZDMV_UITests: XCTestCase {
 
         let app = XCUIApplication()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        // In UI tests it’s important to set the initial state - such as
+        // interface orientation - required for your tests before they run.
+        // The setUp method is a good place to do this.
         setupSnapshot(app)
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        // UI tests must launch the application that they test.
+        // Doing this in setup will make sure it happens for each test method.
         app.launch()
     }
     
@@ -47,7 +50,7 @@ class AZDMV_UITests: XCTestCase {
         saveScreenshot("1. Manual Section Web View")
         app.navigationBars.buttons["Manuals"].tap()
         
-        XCUIApplication().tabBars.buttons["Quiz"].tap()
+        app.tabBars.buttons["Quiz"].tap()
         saveScreenshot("2. Quizzes Table View")
         
         let q1502 = app.tables.cells.containing(.staticText, identifier:"1502").element(boundBy: 0)
@@ -68,6 +71,10 @@ class AZDMV_UITests: XCTestCase {
         app.tables.staticTexts["Special conditions or hazards ahead."].tap()
         saveScreenshot("5. Correct Answer")
         app.otherElements["Close"].tap()
+
+        app.tabBars.buttons["Settings"].tap()
+        app.tables.staticTexts["Alternative App Icons"].tap()
+        saveScreenshot("6. Alternative Icons")
     }
     
     private static var _screenshotID = 0
